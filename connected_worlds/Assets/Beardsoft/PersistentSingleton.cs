40a508ab81c7930b80899dc3eac76e1364e81783
Cory Huckeba <cory@th.ru.st>
2014-08-23 01:00:29 -0400

    Added Beardsoft folder

---
+++ connected_worlds/Assets/Beardsoft/PersistentSingleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Beardsoft.Singleton{
+
+	public class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour {
+
+		protected override void Awake(){
+			base.Awake();
+			if (this != _instance)
+				return;
+
+			Object.DontDestroyOnLoad(this.gameObject);
+		}
+	}
+}
---
+++ connected_worlds/Assets/Beardsoft/Singleton.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Beardsoft.Singleton{
+	
+	public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+		#region Properties and Variables
+
+		private static bool _applicationIsQuitting;
+		protected static bool applicationIsQuitting { get { return _applicationIsQuitting; } }
+
+		private static object _lock = new object();
+
+		protected static T _instance;
+		public static T instance{
+
+			get{
+				if(applicationIsQuitting){
+					Debug.LogWarning("Tried to create instance of " + typeof(T) + " during application quit.");
+					return null;
+				}
+
+				lock(_lock){
+					if(_instance == null){
+
+						_instance = (T) FindObjectOfType(typeof(T));
+					
+						//Check to see if we've created multiple singletons
+						if (FindObjectsOfType(typeof(T)).Length > 1){}
+
+						//If we haven't created any singletons yet let's make one :D
+						if(_instance == null){
+							string singletonName = "Singleton(" + typeof(T).Name + ")";
+							_instance = new GameObject(singletonName).AddComponent<T>();
+						}
+					}
+
+					return _instance;
+				}
+			}
+
+		}
+
+		#endregion
+
+		#region MonoBehavior Implementation
+
+		protected virtual void Awake(){
+			if(_instance == null){
+				_instance = this as T;
+			}
+			else{
+				Debug.LogWarning("Duplicate " + this.name + ". Destroying one");
+				Destroy(gameObject);
+			}
+		}
+
+		protected virtual void OnApplicationQuit(){
+			_applicationIsQuitting = true;
+		}
+
+		#endregion
+	}
+}
