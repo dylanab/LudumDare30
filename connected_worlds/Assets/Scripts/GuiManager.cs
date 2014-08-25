d1dfecff7804d18ee34f7dde7f9aca0b6f9e88dd
Dylan Bowne <dylanab@uga.edu>
2014-08-24 23:55:41 -0400

    More gui work and added LaneBuilder

--- connected_worlds/Assets/Scripts/GuiManager.cs
+++ connected_worlds/Assets/Scripts/GuiManager.cs
@@ -9,6 +9,7 @@ public class GuiManager : Singleton<GuiManager>
 
     public GameObject target;
     public GameObject starPrefab;
+    public GameObject SpaceLane; 
     public GUISkin guiSkin;
     public List<GameObject> systems;
     public Camera camera;
@@ -52,6 +53,7 @@ public class GuiManager : Singleton<GuiManager>
         {
             //test
             sysInfo = obj.GetComponent<SystemControllerTest>();
+            target = obj;
             objectName = sysInfo.name;
             objectType = "System";
             objectTexture = sysInfo.renderer.material.GetTexture("_MainTex");
@@ -86,7 +88,7 @@ public class GuiManager : Singleton<GuiManager>
         GUI.skin = currentSkin;
 
         //draw the labels of systems that are on screen
-        if (camera.fieldOfView < 60)
+        if (Camera.main.fieldOfView < 60)
         {
             for (int i = 0; i < systems.Count; i++)
             {
@@ -94,14 +96,23 @@ public class GuiManager : Singleton<GuiManager>
                 SystemControllerTest systemInfo = systems[i].GetComponent<SystemControllerTest>();
                 Vector3 systemPos = camera.WorldToScreenPoint(systems[i].transform.position);
                 float yPos = camera.pixelHeight - systemPos.y;
+                string buildingType = systemInfo.buildingType;
+                Texture icon = null;
+                if (buildingType == "production")
+                {
+                    icon = productionIcon;
+                } else if(buildingType == "mining") {
+                    icon = miningIcon;
+                }
+                
 
                 if (systemInfo.buildingType == "production")
                 {
-                    GUI.Label(new Rect(systemPos.x - (size.x) - 6, yPos - (size.y) - 33, 30, 30), productionIcon, "systemName");
+                    GUI.Label(new Rect(systemPos.x - (size.x) - 6, yPos - (size.y) - 33, 30, 30), icon, "systemName");
                 }
                 else if (systemInfo.buildingType == "mining")
                 {
-                    GUI.Label(new Rect(systemPos.x - (size.x) - 6, yPos - (size.y) - 33, 30, 30), productionIcon, "systemName");
+                    GUI.Label(new Rect(systemPos.x - (size.x) - 6, yPos - (size.y) - 33, 30, 30), icon, "systemName");
                 }
                 //GUI.Label(new Rect(systemPos.x , yPos - 50, 50, 25), " 5 ", "systemName");
                 GUI.Label(new Rect(systemPos.x - 50, yPos + 20, 100, 25), systemInfo.name, "systemName");
@@ -111,37 +122,44 @@ public class GuiManager : Singleton<GuiManager>
 
         //draw main HUD
 
-
-        
-        //GUI.skin = currentSkin
         menuBarWidth = Screen.width - menuBoxWidth;
 
         GUI.Box(new Rect(0 + menuBoxWidth, Screen.height - menuBarHeight, menuBarWidth, menuBarHeight), "");
-
 
         if (objectType == "System")
         {
             //if there isn't a battle here
 
+            GUI.Label(new Rect(0 + menuBoxWidth + (menuBarWidth * .55f), Screen.height - (menuBoxHeight * .8f) + 20, 100, 40), "Build Facilities", "blanklabel");
+            GUI.Label(new Rect(0 + menuBoxWidth + (menuBarWidth * .25f), Screen.height - (menuBoxHeight * .8f) + 20, 100, 40), "Build SpaceLanes", "blanklabel");
+            if (GUI.Button(new Rect(0 + menuBoxWidth + (menuBarWidth * .25f), Screen.height - (menuBoxHeight * .5f) + 40 + 10, 200, 30), "Create a Space Lane -  50 metal"))
+            {
+                LaneBuilder laneBuilder = this.gameObject.AddComponent<LaneBuilder>();
+                LineRenderer lineRenderer = this.gameObject.AddComponent<LineRenderer>();
+                laneBuilder.placeLaneStart(target, lineRenderer, SpaceLane);
+            }
+
             if (sysInfo.buildingType == "")
             {
-                GUI.Label(new Rect(0 + menuBoxWidth + (menuBarWidth * .55f), Screen.height - (menuBoxHeight * .8f) + 20, 100, 40), "Facilities", "blanklabel");
                 if (GUI.Button(new Rect(0 + menuBoxWidth + (menuBarWidth * .55f), Screen.height - (menuBoxHeight * .5f) + 10, 200, 30), "Create a Factory -  50 metal"))
                 {
-                    Debug.Log("Building a fockin production facility");
+
                     sysInfo.buildBuilding("production");
+                   
+
+                    //objectTexture = productionIcon;
 
                 }
 
                 if (GUI.Button(new Rect(0 + menuBoxWidth + (menuBarWidth * .55f), Screen.height - (menuBoxHeight * .5f) + 40 + 10, 200, 30), "Create a Mine -  50 metal"))
                 {
-                    Debug.Log("Building a fockin mining facility");
                     sysInfo.buildBuilding("mining");
+                    //objectTexture = miningIcon;
                 }
             }
             else if (sysInfo.buildingType == "production")
             {
-                GUI.Label(new Rect(0, Screen.height - menuBoxHeight, menuBoxWidth, menuBoxHeight), "");
+                GUI.Label(new Rect(0 + menuBoxWidth + (menuBarWidth * .55f), Screen.height - (menuBoxHeight * .5f) + 10, 200, 30), "building level : " + sysInfo.buildingLevel, "blanklabel");
                 //if (GUI.Button(new Rect(10, 70, 50, 30), ""))
                 //{
                     //Debug.Log("Clicked the button with text");
@@ -149,7 +167,7 @@ public class GuiManager : Singleton<GuiManager>
             }
             else if (sysInfo.buildingType == "mining")
             {
-                GUI.Label(new Rect(0, Screen.height - menuBoxHeight, menuBoxWidth, menuBoxHeight), "");
+                GUI.Label(new Rect(0 + menuBoxWidth + (menuBarWidth * .55f), Screen.height - (menuBoxHeight * .5f) + 10, 200, 30), "building level : " + sysInfo.buildingLevel, "blanklabel");
                 //if (GUI.Button(new Rect(10, 70, 50, 30), ""))
                 //{
                    // Debug.Log("Clicked the button with text");
---
+++ connected_worlds/Assets/Scripts/LaneBuilder.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using Beardsoft.Singleton;
+
+public class LaneBuilder : Singleton<LaneBuilder>
+{
+    private bool setup = false;
+    private int maxRange = 5;
+    private LineRenderer lineRenderer;
+    private GameObject SpaceLane;
+
+    public GameObject firstSystem;
+    public GameObject secondSystem;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (setup)
+        {
+            Vector3 mousepos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            //Debug.Log("Mousepos : " + mousepos.x + ", " + mousepos.y + ", " + mousepos.z);
+            lineRenderer.SetPosition(1, mousepos);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                mousepos.z = 0f;
+                RaycastHit2D hit = Physics2D.CircleCast(mousepos, 1, Vector2.right);
+                if (hit.transform != null)
+                {
+                    secondSystem = hit.transform.gameObject;
+                    GameObject lane = Instantiate(SpaceLane, firstSystem.transform.position, Quaternion.identity) as GameObject;
+                    LaneController laneController = lane.GetComponent<LaneController>();
+                    //laneController.system1 = firstSystem.gameObject;
+
+                    Debug.Log(hit.transform.gameObject.name);
+                }
+                
+            }
+        }
+	}
+
+    public void placeLaneStart(GameObject system, LineRenderer line, GameObject Lane)
+    {
+        SpaceLane = Lane;
+        lineRenderer = line;
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        //lineRenderer.SetColors(c1, c2);
+        lineRenderer.SetWidth(0.2F, 0.2F);
+        lineRenderer.SetVertexCount(2);
+        firstSystem = system;
+        lineRenderer.SetPosition(0, firstSystem.transform.position);
+        setup = true;
+    }
+}
